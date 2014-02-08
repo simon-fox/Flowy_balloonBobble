94e6a31d2a9101ab0f2f21d3b7656147a79dc059
simon-fox <thiswayin@gmail.com>
2014-02-08 12:26:17 +0000

    storing contacts in array correctly

--- newtodo.js
+++ newtodo.js
@@ -1,8 +1,8 @@
 
 
-- set up level where you shoot balloons by inflating them 
+//- set up level where you shoot balloons by inflating them 
 	//- single balloon on screen
-	- when inflated, replace 
+	//- when inflated, replace 
 	//- Bounding box to keep stuff in
 
 - add in windVector stuff for directing the balloon
@@ -11,6 +11,9 @@
 //- switch to smaller balloons? 
 	//- will have to edit png myself for smaller balloons of different colors
 	//- then switch ballon.js around to have smaller balloons
+
+- destroy rope from ballons which have been inflated 
+
 
 - have balloons pop when in groups of three of same color
 	- some kind of contacts array for each balloon/item
@@ -27,6 +30,11 @@
 			- if two of same color as self
 				- explode 
 
-
+- Or have ballons pop when they take damage
+	- for every contact: 
+		- count hit number on item contacting
+		- add to hits on balloon
+		- when a preset number is reached, explode
+		- bigger items deliver more hits
 
 
