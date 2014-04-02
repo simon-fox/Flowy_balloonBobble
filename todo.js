4af21d120dc2178908c08d0713ffe7762aa8b48a
simon-fox <thiswayin@gmail.com>
2014-04-02 10:22:35 +0100

    updated todo

--- todo.js
+++ todo.js
@@ -4,16 +4,26 @@
 //////////////////////////////
 
 
-- refactor balloon js into item js abstract factory
-	- what does an item do?
-	- subclasses 
-		- dispenser, bomb, special
-	- do we need rope segments any more? 
-		- could replace them with single distance joint?
+//- refactor balloon js into item js abstract factory
+//	- what does an item do?
+//	- subclasses 
+//		- dispenser, bomb, special
+//	- do we need rope segments any more? 
+//		- could replace them with single distance joint?
 
+- How to handle tower blocks falling?
+- b2d or standard impact?
+- is physics ever used?
+- IF b2d
+	- make sure they become static bodies upon falling 
+		- or no longer b2d bodies at all?
+	- spawn in perfect position to fall in lane?
+		- from above screen edges?
+	- or engage some kind of snapping?
+	- or accept uneven lanes ( breaks tetris mech.. )
 
-
-
+- to do tetris effect
+	- will need 
 
 
 
